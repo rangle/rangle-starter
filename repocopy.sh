cd8b1af9b29f3fa6da1d1b2a5bbc7a69f07ca804
Seth Davenport <seth@rangle.io>
2016-01-27 21:40:12 -0500

    Added code to update the project name in config files.
    Currently updates the `name` field in `package.json` and `app.json`.

--- repocopy.sh
+++ repocopy.sh
@@ -14,18 +14,32 @@ regex="[:\/](.+)\/(.+)\.git$"
 if [[ $destRepoUrl =~ $regex ]]; then
     org=${BASH_REMATCH[1]}
     folder=${BASH_REMATCH[2]}
-    echo "org: $org folder: $folder"
 else
     echo "Unable to parse destination repo URL: $destRepoUrl."
     echo "Make sure it has the form git@github.org:org/repo-name.git"
     exit -2
 fi
 
+# Clone the starter repo for the specified techstack and re-initialize it as
+# a new repo with correct remotes.
 git clone git@github.com:rangle/$techStack --depth=1 -o $yourGithubName $folder
 cd $folder
 rm -rf .git
 git init
 git remote add upstream $destRepoUrl
 git remote add origin ${destRepoUrl/$org/$yourGithubName}
+
+
+# Update the project name in package.json, app.json, etc.
+nameRegex='"name"[[:space:]]*:[[:space:]]*".*"'
+nameSub="\"name\": \"$folder\""
+
+for file in package.json app.json
+do
+  mv $file $file.bak
+  cat $file.bak | sed -e "s/$nameRegex/$nameSub/" > $file
+  rm $file.bak
+done
+
 git add --all
 git commit -m "Initial commit from source: $srcRepoUrl"
